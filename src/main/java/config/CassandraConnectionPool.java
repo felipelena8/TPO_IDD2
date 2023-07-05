24620dca1d8845c44228e7616286df9236226f1b
lucashmunoz <lucashmunoz@gmail.com>
2023-07-05 02:21:23 -0300

    agregar-campos-list-logs

--- src/main/java/config/CassandraConnectionPool.java
+++ src/main/java/config/CassandraConnectionPool.java
@@ -1,13 +1,9 @@
 package config;
 
 import com.datastax.oss.driver.api.core.CqlSession;
-import com.datastax.oss.driver.api.core.cql.ResultSet;
-import com.datastax.oss.driver.api.core.cql.Row;
 import dtos.LogDTO;
 
 import java.nio.file.Paths;
-import java.sql.Timestamp;
-import java.util.Date;
 
 public class CassandraConnectionPool {
 
@@ -17,7 +13,8 @@ public class CassandraConnectionPool {
 
     private static CassandraConnectionPool instancia = null;
 
-    private CassandraConnectionPool(){}
+    private CassandraConnectionPool() {
+    }
 
     public static CassandraConnectionPool getInstancia() {
         if (instancia == null) {
@@ -27,40 +24,55 @@ public class CassandraConnectionPool {
     }
 
 
-    public static void connect(){
+    public static void connect() {
         session = CqlSession.builder()
                 .withCloudSecureConnectBundle(Paths.get("src/main/java/config/secure-connect.zip"))
-                .withAuthCredentials("QtWropsYScUHfXSeteRKXZzy","4.qUoUNF0rtbQAIz79eFeOXnTDyutj_i-qZbOuPSGzY2AJ8b4TusL,ODWu4NfLpfj7FbCNzfKw1,DveddJr2vnPQhyw4hXj9rd4Z.UN6+TTigxxNuqIMUZQhbMpuj624")
+                .withAuthCredentials("QtWropsYScUHfXSeteRKXZzy", "4.qUoUNF0rtbQAIz79eFeOXnTDyutj_i-qZbOuPSGzY2AJ8b4TusL,ODWu4NfLpfj7FbCNzfKw1,DveddJr2vnPQhyw4hXj9rd4Z.UN6+TTigxxNuqIMUZQhbMpuj624")
                 .build();
     }
 
-    public void borrarDatosTabla(){
-        if(session==null)
+    public void borrarDatosTabla() {
+        if (session == null)
             connect();
 
         session.execute("truncate logs.producto_logs;");
 
     }
 
-    public void crearRegistroUPDATE(LogDTO registroDTO){
-        if(session==null)
+    public void crearRegistroUPDATE(LogDTO registroDTO) {
+        if (session == null)
             connect();
 
-        session.execute("INSERT INTO logs.producto_logs(tipo_registro, codigo, prev_precio, prev_stock, prev_descripcion, new_precio, new_stock, new_descripcion, datetime ) VALUES ( 'UPDATE', " + registroDTO.getCodigo() + ", " + registroDTO.getPrev_precio() + ", " + registroDTO.getPrev_stock() + ", '" + registroDTO.getPrev_descripcion() + "', " + registroDTO.getNew_precio() + ", " + registroDTO.getNew_stock() + ", '" + registroDTO.getNew_descripcion() + "', " + "toTimeStamp(now()));");
+        String prev_imagenesUrl = registroDTO.getPrev_imagenesUrl().size() > 0 ? "'" + String.join("', '", registroDTO.getPrev_imagenesUrl()) + "'" : "";
+        String prev_videosUrl = registroDTO.getPrev_videosUrl().size() > 0 ? "'" + String.join("', '", registroDTO.getPrev_videosUrl()) + "'" : "";
+        String prev_comentarios = registroDTO.getPrev_comentarios().size() > 0 ? "'" + String.join("', '", registroDTO.getPrev_comentarios()) + "'" : "";
 
+        String new_imagenesUrl = registroDTO.getNew_imagenesUrl().size() > 0 ? "'" + String.join("', '", registroDTO.getNew_imagenesUrl()) + "'" : "";
+        String new_videosUrl = registroDTO.getNew_videosUrl().size() > 0 ? "'" + String.join("', '", registroDTO.getNew_videosUrl()) + "'" : "";
+        String new_comentarios = registroDTO.getNew_comentarios().size() > 0 ? "'" + String.join("', '", registroDTO.getNew_comentarios()) + "'" : "";
+
+        session.execute("INSERT INTO logs.producto_logs(tipo_registro, codigo, prev_precio, prev_stock, prev_descripcion, prev_imagenes, prev_videos, prev_comentarios, new_precio, new_stock, new_descripcion, new_imagenes, new_videos, new_comentarios, datetime ) VALUES ( 'UPDATE', " + registroDTO.getCodigo() + ", " + registroDTO.getPrev_precio() + ", " + registroDTO.getPrev_stock() + ", '" + registroDTO.getPrev_descripcion() + "', [" + prev_imagenesUrl + "], [" + prev_videosUrl + "], [" + prev_comentarios + "], " + registroDTO.getNew_precio() + ", " + registroDTO.getNew_stock() + ", '" + registroDTO.getNew_descripcion() + "', [" + new_imagenesUrl + "], [" + new_videosUrl + "], [" + new_comentarios + "], " + "toTimeStamp(now()));");
     }
 
-    public void crearRegistroCREATE(LogDTO registroDTO){
-        if(session==null)
+    public void crearRegistroCREATE(LogDTO registroDTO) {
+        if (session == null)
             connect();
 
-        session.execute("INSERT INTO logs.producto_logs(tipo_registro, codigo, new_precio, new_stock, new_descripcion, datetime ) VALUES ( 'CREATE', " + registroDTO.getCodigo() + ", " + registroDTO.getNew_precio() + ", " + registroDTO.getNew_stock() + ", '" + registroDTO.getNew_descripcion() + "', " + "toTimeStamp(now()));");
+        String new_imagenesUrl = registroDTO.getNew_imagenesUrl().size() > 0 ? "'" + String.join("', '", registroDTO.getNew_imagenesUrl()) + "'" : "";
+        String new_videosUrl = registroDTO.getNew_videosUrl().size() > 0 ? "'" + String.join("', '", registroDTO.getNew_videosUrl()) + "'" : "";
+        String new_comentarios = registroDTO.getNew_comentarios().size() > 0 ? "'" + String.join("', '", registroDTO.getNew_comentarios()) + "'" : "";
+
+        session.execute("INSERT INTO logs.producto_logs(tipo_registro, codigo, new_precio, new_stock, new_descripcion, new_imagenes, new_videos, new_comentarios, datetime ) VALUES ( 'CREATE', " + registroDTO.getCodigo() + ", " + registroDTO.getNew_precio() + ", " + registroDTO.getNew_stock() + ", '" + registroDTO.getNew_descripcion() + "', [" + new_imagenesUrl + "], [" + new_videosUrl + "], [" + new_comentarios + "], " + "toTimeStamp(now()));");
     }
 
-    public void crearRegistroDELETE(LogDTO registroDTO){
-        if(session==null)
+    public void crearRegistroDELETE(LogDTO registroDTO) {
+        if (session == null)
             connect();
 
-        session.execute("INSERT INTO logs.producto_logs(tipo_registro, codigo, prev_precio, prev_stock, prev_descripcion, datetime ) VALUES ( 'DELETE', " + registroDTO.getCodigo() + ", " + registroDTO.getPrev_precio() + ", " + registroDTO.getPrev_stock() + ", '" + registroDTO.getPrev_descripcion() + "', " + "toTimeStamp(now()));");
+        String prev_imagenesUrl = registroDTO.getPrev_imagenesUrl().size() > 0 ? "'" + String.join("', '", registroDTO.getPrev_imagenesUrl()) + "'" : "";
+        String prev_videosUrl = registroDTO.getPrev_videosUrl().size() > 0 ? "'" + String.join("', '", registroDTO.getPrev_videosUrl()) + "'" : "";
+        String prev_comentarios = registroDTO.getPrev_comentarios().size() > 0 ? "'" + String.join("', '", registroDTO.getPrev_comentarios()) + "'" : "";
+
+        session.execute("INSERT INTO logs.producto_logs(tipo_registro, codigo, prev_precio, prev_stock, prev_descripcion, prev_imagenes, prev_videos, prev_comentarios, datetime ) VALUES ( 'DELETE', " + registroDTO.getCodigo() + ", " + registroDTO.getPrev_precio() + ", " + registroDTO.getPrev_stock() + ", '" + registroDTO.getPrev_descripcion() + "', [" + prev_imagenesUrl + "], [" + prev_videosUrl + "], [" + prev_comentarios + "], " + "toTimeStamp(now()));");
     }
 }
--- src/main/java/logger/UpdateLog.java
+++ src/main/java/logger/UpdateLog.java
@@ -2,19 +2,30 @@ package logger;
 
 import config.CassandraConnectionPool;
 import dtos.LogDTO;
+import java.util.List;
 
 public class UpdateLog implements TipoLog {
     @Override
     public void registrar(RegistroLog registro) {
+
+        List<String> prev_comentarios = registro.getEstadoAnterior().getComentarios().stream().map(comentario -> comentario.toString()).toList();
+        List<String> new_comentarios = registro.getNuevoEstado().getComentarios().stream().map(comentario -> comentario.toString()).toList();
+
         LogDTO logDTO = new LogDTO(
                 "UPDATE",
                 registro.getNuevoEstado().getId(),
                 registro.getEstadoAnterior().getPrecio(),
                 registro.getEstadoAnterior().getStock(),
                 registro.getEstadoAnterior().getDescripcion(),
+                registro.getEstadoAnterior().getImagenesUrl(),
+                registro.getEstadoAnterior().getVideosUrl(),
+                prev_comentarios,
                 registro.getNuevoEstado().getPrecio(),
                 registro.getNuevoEstado().getStock(),
-                registro.getNuevoEstado().getDescripcion()
+                registro.getNuevoEstado().getDescripcion(),
+                registro.getNuevoEstado().getImagenesUrl(),
+                registro.getNuevoEstado().getVideosUrl(),
+                new_comentarios
         );
 
         CassandraConnectionPool pool = CassandraConnectionPool.getInstancia();
