d657673fc0cb8b1a4789dedf1038b38fdee84a21
unknown <you@example.com>
2023-07-05 17:28:52 -0300

    fran

---
+++ src/main/java/repositories/FacturaRepository.java
@@ -0,0 +1,32 @@
+package repositories;
+
+import config.ObjectDBConnectionPool;
+import lombok.Getter;
+import models.Factura;
+import models.Producto;
+import models.Usuario;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
+
+public class FacturaRepository {
+    @Getter
+    private EntityManager em = ObjectDBConnectionPool.getInstancia().getConnection();
+
+
+    public void save(Factura factura) {
+        em.getTransaction().begin();
+        em.persist(factura);
+        em.getTransaction().commit();
+    }
+    public List<Factura> readAll(){
+        Query query = em.createQuery("SELECT u FROM Factura u");
+        List<Factura>  facturas = query.getResultList();
+        return  facturas;
+    }
+
+
+
+
+}
